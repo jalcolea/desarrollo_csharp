7edeb83dd2d8642c5ebc0e80053e0debbc0c3110
jalcolea <jalcoleam@gmail.com>
2021-03-23 23:00:17 +0100

    añadidos eventos de pulsacion a los botones y contenido a los botones numericos

--- wpf/calculator_grid_xaml/calculator/calculator/MainWindow.xaml.cs
+++ wpf/calculator_grid_xaml/calculator/calculator/MainWindow.xaml.cs
@@ -23,7 +23,104 @@ namespace calculator
         public MainWindow()
         {
             InitializeComponent();
-            ResultLbl.Content = "1";
+        //    ResultLbl.Content = "1";
+        }
+
+        private void AddContentToScreen(int number)
+        {
+            if (Convert.ToInt16(ResultLbl.Content) == 0) ResultLbl.Content = "";
+            ResultLbl.Content += number.ToString();
+        }
+        private void AcBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ResultLbl.Content = "0";
+        }
+
+        private void SignBtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PercentageBtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DivisionBtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SevenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void EigthBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void NineBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void MultiplicationBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FourBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void FiveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void SixBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void MinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void TwoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void ThreeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void PlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZeroBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddContentToScreen(Convert.ToInt16((sender as Button).Content));
+        }
+
+        private void DotBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EqualBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
